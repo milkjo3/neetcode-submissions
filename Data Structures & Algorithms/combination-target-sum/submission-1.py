8875accd8422fc49fd0fa34d07365702be6b6dcf
Joseph Milliken <151595627+milkjo3@users.noreply.github.com>
2026-06-15 14:30:38 -0600

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # TC: O(n^(target / min(nums)))
+        # We explore a recursion tree with branching factor n and
+        # maximum depth target / min(nums).
+
+        # SC: O(target / min(nums))
+        # Maximum recursion depth and path length.
+        # goal - generate all combination sums
+        # constraint - must add up to target
+        # choice - a number in nums
+
+        sol = []
+        path = []
+
+        def backtrack(start, total):
+            # Add the solution to the target.
+            if total == target:
+                sol.append(path.copy())
+                return
+
+            # Undo that choice, it added up to over the target's value.
+            if total >= target:
+                return
+
+            # Consider numbers from index start onward only (preventing duplicated combination sums).
+            for i in range(start, len(nums)):
+
+                # Make a choice (add a number).
+                path.append(nums[i])
+
+                # Explore all paths after making the choice.
+                # Passing i instead of i + 1, because we can make the same choice multiple times.
+                backtrack(i, total + nums[i])
+
+                # Undo the choice.
+                path.pop()
+
+        backtrack(0,0)        
+        return sol
